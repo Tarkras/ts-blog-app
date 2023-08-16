29632126433039ab545875f0d3c75f4cabccf451
Dídac Ortega Escalera <didacoe@gmail.com>
2023-08-16 10:52:57 +0200

    First commit

---
+++ src/components/Timeline.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import type { Ref } from 'vue'
+
+  const periods: Array<string> = ['Today', 'This week', 'This month']
+
+  let selectedPeriod: Ref<string> = ref('Today')
+
+  const selectPeriod = (period: string): void => {
+    selectedPeriod.value = period
+  }
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    {{ selectedPeriod }}
+    <span class="panel-tabs">
+      <a
+        v-for="(period, index) in periods"
+        :key="index"
+        href="javascript:void(0)"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
+</template>
+
+<style></style>
