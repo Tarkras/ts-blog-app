770c7d4be5f79ecaf683b349e59677845d9f6e11
Dídac Ortega Escalera <didacoe@gmail.com>
2023-08-22 15:05:33 +0200

    Created component timeline item

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,29 +1,62 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import type { Ref } from 'vue'
+  import { DateTime } from 'luxon'
+  import { ref, computed } from 'vue'
+  import type { ComputedRef, Ref } from 'vue'
 
-  const periods: Array<string> = ['Today', 'This week', 'This month']
+  import { TimelinePost, today, thisWeek, thisMonth } from '../interfaces/posts'
+  import TimelineItem from './TimelineItem.vue'
 
-  let selectedPeriod: Ref<string> = ref('Today')
+  const periods = ['Today', 'This week', 'This month'] as const
 
-  const selectPeriod = (period: string): void => {
+  type Period = typeof periods[number]
+
+  const selectedPeriod: Ref<Period> = ref('Today')
+
+  const selectPeriod = (period: Period): void => {
     selectedPeriod.value = period
   }
+
+  const posts: ComputedRef<TimelinePost[]> = computed<TimelinePost[]>(() => {
+    return [today, thisWeek, thisMonth]
+    .map(post => {
+      return {
+        ...post,
+        created: DateTime.fromISO(post.created)
+      }
+    })
+    .filter(post => {
+      if (selectedPeriod.value === 'Today') {
+        return post.created >= DateTime.now().minus({ day: 1 })
+      }
+
+      if (selectedPeriod.value === 'This week') {
+        return post.created >= DateTime.now().minus({ week: 1})
+      }
+
+      return post
+    })
+  }) 
 </script>
 
 <template>
   <nav class="is-primary panel">
-    {{ selectedPeriod }}
     <span class="panel-tabs">
       <a
         v-for="(period, index) in periods"
         :key="index"
         href="javascript:void(0)"
+        :class="{ 'is-active': period === selectedPeriod}"
         @click="selectPeriod(period)"
       >
         {{ period }}
       </a>
     </span>
+
+    <TimelineItem
+      v-for="post of posts"
+      :key="post.id"
+      :post="post"
+    />
   </nav>
 </template>
 
---
+++ src/interfaces/posts.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+
+export interface Post {
+  id: string
+  title: string
+  created: any // To check the correct type
+}
+
+export interface TimelinePost extends Omit<Post, 'created'> {
+  created: DateTime // Gives error due to no being able to use as type. Could be used either way.
+}
+
+export const today: Post = {
+  id: '1',
+  title: 'Today',
+  created: DateTime.now().toISO()
+}
+
+export const thisWeek: Post = {
+  id: '1',
+  title: 'This week',
+  created: DateTime.now().minus({ days: 5}).toISO()
+}
+
+export const thisMonth: Post = {
+  id: '1',
+  title: 'This month',
+  created: DateTime.now().minus({ weeks: 3}).toISO()
+}
